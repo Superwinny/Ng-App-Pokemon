e0b10f4c662593f5904d807858d6c599e5a7fdc3
Superwinny <dos.davide91@gmail.com>
2022-07-15 13:57:07 +0200

    Commit upgrade
    Modification Detail.pokemon.component + ajout code html pour la page

--- ng-app-pokemon/src/app/detail-pokemon/detail-pokemon.component.ts
+++ ng-app-pokemon/src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
-  styles: [
-  ]
+
 })
 export class DetailPokemonComponent implements OnInit {
 
-  constructor() { }
+  pokemonList: Pokemon[];
+  pokemon: Pokemon | undefined;
 
-  ngOnInit(): void {
+
+  constructor(private roter: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string | null = this.roter.snapshot.paramMap.get('id');
+    if (pokemonId) {
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    }
+
   }
 
 }
--- ng-app-pokemon/src/app/liste-pokemon/liste-pokemon.component.ts
+++ ng-app-pokemon/src/app/liste-pokemon/liste-pokemon.component.ts
@@ -5,10 +5,10 @@ import { Pokemon } from '../pokemon';
 @Component({
   selector: 'app-liste-pokemon',
   templateUrl: './liste-pokemon.component.html',
- 
-})
-export class ListePokemonComponent  {
-  pokemonList: Pokemon[]=POKEMONS;
 
-  
+})
+export class ListePokemonComponent {
+  pokemonList: Pokemon[] = POKEMONS;
+
+
 }
