b05ce5cc11846779e6b1a5a7ebe21372955ceb0b
Dos Santos Davide <dos.davide91@gmail.com>
2022-07-21 11:08:25 +0200

    Commit New Module
    Création du module pokemon + modification des fichiers

---
+++ ng-app-pokemon/src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListePokemonComponent } from './liste-pokemon/liste-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { RouterModule, Routes } from '@angular/router';
+
+const pokemonRoutes: Routes = [
+  { path: 'pokemons', component: ListePokemonComponent },
+  { path: 'pokemons/:id', component: DetailPokemonComponent },
+
+];
+
+
+@NgModule({
+  declarations: [
+    ListePokemonComponent,
+    DetailPokemonComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(pokemonRoutes)
+  ]
+})
+export class PokemonModule { }
