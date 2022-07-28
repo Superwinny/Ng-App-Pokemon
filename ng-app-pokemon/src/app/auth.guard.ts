f8a567c887c270a47d64e944dc98f8f743bd2cd4
Dos Santos Davide <dos.davide91@gmail.com>
2022-07-28 14:19:53 +0200

    Commit Guard
    Ajout auth guard + service

---
+++ ng-app-pokemon/src/app/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(){
+    console.log('Bien été appelé !');
+    
+    return true;
+  }
+   
+    
+}
+  
+
---
+++ ng-app-pokemon/src/app/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isLoggedIn: boolean = false;
+  redirectUrl : string;
+
+
+  login(){
+
+  }
+
+  logout(){
+
+  }
+}
