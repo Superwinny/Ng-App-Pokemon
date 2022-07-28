8ccee1aa0ff3d850683cc40b1a0629fa39e28614
Dos Santos Davide <dos.davide91@gmail.com>
2022-07-28 11:11:27 +0200

    Commit

--- ng-app-pokemon/src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ ng-app-pokemon/src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,27 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../pokemon';
 import { Router } from '@angular/router';
-import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { PokemonService } from '../pokemon.service';
 @Component({
   selector: 'app-search-pokemon',
   templateUrl: './search-pokemon.component.html',
-  
+
 })
 export class SearchPokemonComponent implements OnInit {
+  searchTerms = new Subject<string>(); // searchTerm sert a construire ce flux de donné
+  pokemons$: Observable<Pokemon[] | any>;
 
-  searchTerms = new Subject <string>(); // searchTerm sert a construire ce flux de donné
-  pokemons$: Observable<Pokemon[]>;
- 
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      //{..."a"..."ab"..."abz"..."ab"..."abc"...}
+      debounceTime(300),
+      //{......"ab"......"ab"..."abc"......}
+      distinctUntilChanged(),
+      //{......"ab"........."abc"......}
+      switchMap((term) => this.pokemonService.searchPokemonList(term)),
+      //{......pokemonList(ab).........pokemonList(abc)......}
+    );
   }
 
-  search(term: string){
+  search(term: string) {
     this.searchTerms.next(term) // Next utilisé pour pousser un elément dans le flux de donné
   }
-  goToDetail(pokemon: Pokemon){
+  goToDetail(pokemon: Pokemon) {
     const link = ['/pokemon', pokemon.id]
     this.router.navigate(link);
   }
